aca07075bd5615dfb9a93f19afe7a9271351a048
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-05-07 18:20:21 +0200

    reorganize code

--- main.go
+++ main.go
@@ -3,37 +3,46 @@ package main
 import (
 	"code.google.com/p/go.net/websocket"
 	"flag"
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 	"text/template"
 )
 
-// flags
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var (
+	//flags
+	addr = flag.String("addr", "localhost:8080", "http service address")
 
-// templates
-var chatTempl = template.Must(template.ParseFiles("chat.html"))
-var indexTempl = template.Must(template.ParseFiles("index.html"))
+	// templates
+	chatTempl  = template.Must(template.ParseFiles("chat.html"))
+	indexTempl = template.Must(template.ParseFiles("index.html"))
+
+	// hub
+	h = hub {
+		broadcast:   make(chan string),
+		register:    make(chan *connection),
+		unregister:  make(chan *connection),
+		connections: make(map[*connection]bool),
+	}
+)
 
 // handlers
 func chatHandler(c http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		var nick string
 
-  if req.Method == "POST" {
-    var nick string
+		if nick = req.FormValue("nickname"); nick != "" {
+			http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
+		} else {
+			nick = randNickname()
+		}
 
-    if nick = req.FormValue("nickname"); nick != "" {
-      http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
-    } else {
-      nick = randNickname()
-    }
+		http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
 
-    http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
-
-  } else { // it's a GET
-    if _, err := req.Cookie("user"); err != nil {
-      http.Redirect(c, req, "/", 403)
-    }
-  }
+	} else { // it's a GET
+		if _, err := req.Cookie("user"); err != nil {
+			http.Redirect(c, req, "/", 403)
+		}
+	}
 
 	chatTempl.Execute(c, req.Host)
 }
@@ -43,10 +52,10 @@ func indexHandler(c http.ResponseWriter, req *http.Request) {
 }
 
 func wsHandler(ws *websocket.Conn) {
-  cookie, _ := ws.Request().Cookie("user")
-  nick := cookie.Value
+	cookie, _ := ws.Request().Cookie("user")
+	nick := cookie.Value
 
-  c := &connection{send: make(chan string, 256), ws: ws, user: nick}
+	c := &connection{send: make(chan string, 256), ws: ws, user: nick}
 	h.register <- c
 	defer func() { h.unregister <- c }()
 	go c.writer()
@@ -58,20 +67,11 @@ func main() {
 
 	go h.run()
 
-  http.HandleFunc("/", indexHandler)
-  http.HandleFunc("/chat", chatHandler)
-  http.Handle("/ws", websocket.Handler(wsHandler)) //GET
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/chat", chatHandler)
+	http.Handle("/ws", websocket.Handler(wsHandler)) //GET
 
-  if err := http.ListenAndServe(*addr, nil); err != nil {
-    log.Fatal("ListenAndServe:", err)
-  }
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
-
-
-/*
-
-func (s *Server) Websocket(route string, httpHandler websocket.Handler) {
-  s.addRoute(route, "GET", httpHandler)
-}
-
-*/
