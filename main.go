684750a6bc4cbce6f6fbe351a5d9aa88ddf34901
Stefano Verna <stefano.verna@welaika.com>
2013-05-09 17:49:32 +0200

    extracted server object

--- main.go
+++ main.go
@@ -1,84 +1,11 @@
 package main
 
-import (
-  "code.google.com/p/go.net/websocket"
-  "flag"
-  "log"
-  "net/http"
-  "text/template"
-  "fmt"
-  "math/rand"
-  "time"
-)
-
-var (
-  //flags
-  addr = flag.String("addr", "localhost:8080", "http service address")
-
-  // templates
-  chatTempl  = template.Must(template.ParseFiles("chat.html"))
-  indexTempl = template.Must(template.ParseFiles("index.html"))
-
-  // hub
-  h = newHub()
-)
-
-func randNickname() string {
-  seed := time.Now().UTC().UnixNano()
-  rand.Seed(seed)
-
-  val := int(seed)
-  if val < 0 {
-    val = -val
-  }
-
-  return fmt.Sprintf("anonymous-%d", (1 + rand.Intn(val)))
-}
-
-// handlers
-func chatHandler(c http.ResponseWriter, req *http.Request) {
-  if req.Method == "POST" {
-    var nick string
-
-    if nick = req.FormValue("nickname"); nick != "" {
-      http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
-    } else {
-      nick = randNickname()
-    }
-
-    http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
-
-  } else { // it's a GET
-    if _, err := req.Cookie("user"); err != nil {
-      http.Redirect(c, req, "/", 403)
-    }
-  }
-
-  chatTempl.Execute(c, req.Host)
-}
-
-func indexHandler(c http.ResponseWriter, req *http.Request) {
-  indexTempl.Execute(c, req.Host)
-}
-
-func wsHandler(ws *websocket.Conn) {
-  cookie, _ := ws.Request().Cookie("user")
-  nick := cookie.Value
-  c := h.registerConnection(nick, ws)
-  c.run()
-}
+import "flag"
 
 func main() {
   flag.Parse()
-
-  h.run()
-
-  http.HandleFunc("/", indexHandler)
-  http.HandleFunc("/chat", chatHandler)
-  http.Handle("/ws", websocket.Handler(wsHandler)) //GET
-
-  if err := http.ListenAndServe(*addr, nil); err != nil {
-    log.Fatal("ListenAndServe:", err)
-  }
+  addr := flag.String("addr", "localhost:8080", "http service address")
+  server := newServer(*addr)
+  server.run()
 }
 
---
+++ server.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+  "code.google.com/p/go.net/websocket"
+  "fmt"
+  "log"
+  "math/rand"
+  "net/http"
+  "text/template"
+  "time"
+)
+
+func randNickname() string {
+  seed := time.Now().UTC().UnixNano()
+  rand.Seed(seed)
+
+  val := int(seed)
+  if val < 0 {
+    val = -val
+  }
+
+  return fmt.Sprintf("anonymous-%d", (1 + rand.Intn(val)))
+}
+
+// handlers
+
+type server struct {
+  chatTemplate *template.Template
+  indexTemplate *template.Template
+  hub *hub
+  addr string
+}
+
+func newServer(addr string) *server {
+  return &server {
+    chatTemplate: template.Must(template.ParseFiles("chat.html")),
+    indexTemplate: template.Must(template.ParseFiles("index.html")),
+    hub: newHub(),
+    addr: addr,
+  }
+}
+
+func (s *server) run() {
+  s.hub.run()
+
+  http.HandleFunc("/", func (c http.ResponseWriter, req *http.Request) {
+    s.indexTemplate.Execute(c, req.Host)
+  })
+
+  http.HandleFunc("/chat", func (c http.ResponseWriter, req *http.Request) {
+    if req.Method == "POST" {
+      var nick string
+
+      if nick = req.FormValue("nickname"); nick != "" {
+        http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
+      } else {
+        nick = randNickname()
+      }
+
+      http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
+
+    } else { // it's a GET
+      if _, err := req.Cookie("user"); err != nil {
+        http.Redirect(c, req, "/", 403)
+      }
+    }
+
+    s.chatTemplate.Execute(c, req.Host)
+  })
+
+  http.Handle("/ws", websocket.Handler(func (ws *websocket.Conn) {
+    cookie, _ := ws.Request().Cookie("user")
+    nick := cookie.Value
+    c := s.hub.registerConnection(nick, ws)
+    c.run()
+  }))
+
+  if err := http.ListenAndServe(s.addr, nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
+}
+
