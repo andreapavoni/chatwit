298ace242a13001bf848bb7a342f825210d7f45e
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-05-10 11:40:03 +0200

    add gorillatoolkit.org/pkg/mux to handle routes

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
   "net/http"
   "text/template"
   "time"
+  "github.com/gorilla/mux"
 )
 
 func randNickname() string {
@@ -43,36 +44,45 @@ func newServer(addr string) *server {
 func (s *server) run() {
   s.hub.run()
 
-  http.HandleFunc("/", func (c http.ResponseWriter, req *http.Request) {
+  router := mux.NewRouter()
+
+  /* router.HandleFunc("/", ProductsHandler).Methods("GET")*/
+
+  router.HandleFunc("/", func (c http.ResponseWriter, req *http.Request) {
     s.indexTemplate.Execute(c, req.Host)
-  })
+  }).Methods("GET")
 
-  http.HandleFunc("/chat", func (c http.ResponseWriter, req *http.Request) {
-    if req.Method == "POST" {
-      var nick string
-
-      if nick = req.FormValue("nickname"); nick != "" {
-        http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
-      } else {
-        nick = randNickname()
-      }
-
-      http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
-
-    } else { // it's a GET
-      if _, err := req.Cookie("user"); err != nil {
-        http.Redirect(c, req, "/", 403)
-      }
+  router.HandleFunc("/chat", func (c http.ResponseWriter, req *http.Request) {
+    if _, err := req.Cookie("user"); err != nil {
+      http.Redirect(c, req, "/", 403)
     }
 
     s.chatTemplate.Execute(c, req.Host)
-  })
 
-  http.Handle("/ws", websocket.Handler(func (ws *websocket.Conn) {
+  }).Methods("GET")
+
+
+  router.HandleFunc("/chat", func (c http.ResponseWriter, req *http.Request) {
+    var nick string
+
+    if nick = req.FormValue("nickname"); nick != "" {
+      http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
+    } else {
+      nick = randNickname()
+    }
+
+    http.SetCookie(c, &http.Cookie{Name: "user", Value: nick})
+    s.chatTemplate.Execute(c, req.Host)
+
+  }).Methods("POST")
+
+  router.Handle("/ws", websocket.Handler(func (ws *websocket.Conn) {
     cookie, _ := ws.Request().Cookie("user")
     nick := cookie.Value
     s.hub.registerConnection(nick, ws)
   }))
+
+  http.Handle("/", router)
 
   if err := http.ListenAndServe(s.addr, nil); err != nil {
     log.Fatal("ListenAndServe:", err)
