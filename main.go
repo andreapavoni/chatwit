cf8c5c60c9a25f0ebe26b913459b6e23a3b2e8f7
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-05-20 16:25:11 +0200

    add command line options support

--- main.go
+++ main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"flag"
+  "fmt"
 )
 
 func main() {
 	flag.Parse()
-	addr := flag.String("addr", "127.0.0.1:8080", "http service address")
+
+  // address
+	address := flag.String("addr", "127.0.0.1:8080", "http service address")
+
+  // oauth
+	key := flag.String("oa-key", "M9MHfTfKDyF5yZM6xueTxg", "OAuth consumer key")
+	secret := flag.String("oa-secret", "1lClcicoUNEKA1pycLLO0Jruo0NA2AgK3KhLFY4jo", "OAuth consumer secret")
+
+	cbHost := flag.String("oa-callback", "http://127.0.0.1:8080", "OAuth callback host")
+  callbackHost := fmt.Sprintf("%s/auth/twitter/callback", *cbHost)
+
+  // session cookie
+	session := flag.String("session-secret", "foobarsecret", "Session secret key")
+
 
 	config := ConfigServer{
-		oauthKey:      "M9MHfTfKDyF5yZM6xueTxg",
-		oauthSecret:   "1lClcicoUNEKA1pycLLO0Jruo0NA2AgK3KhLFY4jo",
-		oauthCallback: "http://127.0.0.1:8080/auth/twitter/callback",
-
-		storeSecret: "foobarquxsecret",
+		oauthKey:      *key,
+		oauthSecret:   *secret,
+		oauthCallback: callbackHost,
+		storeSecret: *session,
 	}
 
 	server := NewServer(&config)
-	server.Run(*addr)
+	server.Run(*address)
 }
