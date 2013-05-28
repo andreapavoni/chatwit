a4a2ed083376bfde60bafe7ffdead8e4f133d37d
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-05-28 16:32:44 +0200

    refactor config params (wip)

--- main.go
+++ main.go
@@ -3,25 +3,29 @@ package main
 import (
   "log"
   "flag"
+  "strings"
 )
 
 func main() {
   file := flag.String("conf", "development.conf.json", "configuration file")
+  port := flag.String("port", "8080", "port to listen")
   flag.Parse()
 
-  var config AppConfig
+  var conf AppConfig
 
-  if  err := ReadJSONConfig((*file), &config); err != nil {
+  if  err := ReadJSONConfig((*file), &conf); err != nil {
     log.Fatal(err)
   }
 
+  listen := conf.Address + ":" + *port
+
   srvConfig := ConfigServer{
-    oauthKey:      config.OAuth.Key,
-    oauthSecret:   config.OAuth.Secret,
-    oauthCallback: config.OAuth.Callback,
-    storeSecret:   config.SessionSecret,
+    oauthKey:      conf.OAuth.Key,
+    oauthSecret:   conf.OAuth.Secret,
+    oauthCallback: strings.Replace(conf.OAuth.Callback, "ADDRESS", listen, -1),
+    storeSecret:   conf.SessionSecret,
   }
 
   server := NewServer(&srvConfig)
-  server.Run(config.Address)
+  server.Run(listen)
 }
